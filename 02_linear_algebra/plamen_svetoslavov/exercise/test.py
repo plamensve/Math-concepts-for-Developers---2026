1952d2d520e03309626c1ec66bfa70adfec13f6c
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2026-02-16 23:22:48 +0200

    ADD: Problem 2

---
+++ 02_linear_algebra/plamen_svetoslavov/exercise/test.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def encode(text):
+    d = deque(list(text.upper()))
+    first_symb = d.popleft()
+    result = []
+
+    current_symbol = f'{first_symb}'
+    while d:
+        symb = d.popleft()
+
+        if current_symbol[-1] != symb:
+            result.append((current_symbol[-1], len(current_symbol)))
+            current_symbol = ''
+            current_symbol += symb
+        else:
+            current_symbol += symb
+
+        if not d:
+            result.append((current_symbol[-1], len(current_symbol)))
+            break
+
+    return ''.join(f"{char}{count}" if count > 1 else char for char, count in result)
+
+
+print(encode('AABCCCDDAaAccchhhww'))
