2b3538bbbe142f9e16ab3133375c3d01f3334dab
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2026-02-22 12:50:41 +0200

    ADD:

--- 02_linear_algebra/plamen_svetoslavov/exercise/test.py
+++ 02_linear_algebra/plamen_svetoslavov/exercise/test.py
@@ -1,6 +1,7 @@
 from collections import deque
 import re
 
+
 def encode(text):
     d = deque(list(text))
     first_symb = d.popleft()
@@ -24,18 +25,5 @@ def encode(text):
     return ''.join(f"{char}{count}" if count > 1 else char for char, count in result)
 
 
-def decode(text):
-    """
-    Decodes the text using run-length encoding
-    """
-    pattern = r"[A-Za-z]\d*"
-    matches = re.findall(pattern, text)
-    result = []
 
-    for i in matches:
-        result.append(i[0] * int(i[1:] if i[1:] else 1))
-
-    return ''.join(result)
-
-
-print(decode("A2BC3XCCDE4"))
+print(encode("аAACABCCS"))
